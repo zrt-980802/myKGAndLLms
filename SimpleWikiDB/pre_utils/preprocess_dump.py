6e2eeaccdf480e55a28d742ae856c84bffa936d7
29557 <295575817@qq.c>
2024-11-09 16:06:23 +0800

    # update

--- SimpleWikiDB/pre_utils/preprocess_dump.py
+++ SimpleWikiDB/pre_utils/preprocess_dump.py
@@ -16,36 +16,28 @@ from preprocess_utils.reader_process import count_lines, read_data
 from preprocess_utils.worker_process import process_data
 from preprocess_utils.writer_process import write_data
 
-
-def get_arg_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', type=str, required=True, help='path to gz wikidata json dump')
-    parser.add_argument('--out_dir', type=str, required=True, help='path to output directory')
-    parser.add_argument('--language_id', type=str, default='en', help='language identifier')
-    parser.add_argument('--processes', type=int, default=90, help="number of concurrent processes to spin off. ")
-    parser.add_argument('--batch_size', type=int, default=10000)
-    parser.add_argument('--num_lines_read', type=int, default=-1,
-                        help='Terminate after num_lines_read lines are read. Useful for debugging.')
-    parser.add_argument('--num_lines_in_dump', type=int, default=-1, help='Number of lines in dump. If -1, we will count the number of lines.')
-    return parser
+input_file = 'D:\data\wikidata-20240101-all.json.gz'
+out_dir = 'D:\data\output\\'
+language_id = 'en'
+processes = '90'
+batch_size = 10000
+num_lines_read = -1
+num_lines_in_dump = -1
 
 
-def main():
+def main(paras):
     start = time.time()
-    args = get_arg_parser().parse_args()
-    print(f"ARGS: {args}")
 
-    out_dir = Path(args.out_dir)
+    out_dir = Path(paras['out_dir'])
     out_dir.mkdir(exist_ok=True, parents=True)
 
-    input_file = Path(args.input_file)
+    input_file = Path(paras['input_file'])
     assert input_file.exists(), f"Input file {input_file} does not exist"
 
-
-    max_lines_to_read = args.num_lines_read
+    max_lines_to_read = paras['num_lines_read']
 
     print("Starting processes")
-    maxsize = 10 * args.processes
+    maxsize = 10 * paras['processes']
 
     # Queues for inputs/outputs
     output_queue = Queue(maxsize=maxsize)
@@ -62,15 +54,15 @@ def main():
 
     write_process = Process(
         target=write_data,
-        args=(out_dir, args.batch_size, output_queue)
+        args=(out_dir, paras['batch_size'], output_queue)
     )
     write_process.start()
 
     work_processes = []
-    for _ in range(max(1, args.processes-2)):
+    for _ in range(max(1, paras['processes'] - 2)):
         work_process = Process(
             target=process_data,
-            args=(args.language_id, work_queue, output_queue)
+            args=(paras['language_id'], work_queue, output_queue)
         )
         work_process.daemon = True
         work_process.start()
@@ -91,4 +83,12 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    args = {
+        'input_file': 'D:\data\wikidata-20240101-all.json.gz',
+        'out_dir': 'D:\data\output\\',
+        'language_id': 'en',
+        'processes': '90',
+        'batch_size': 10000,
+        'num_lines_read': -1,
+        'num_lines_in_dump': -1}
+    main(args)
